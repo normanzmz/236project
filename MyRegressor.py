33e6a7f7483975b3a6a011a3b987419d949336c6
normanzmz <mzhu@wpi.edu>
2022-11-05 13:13:24 -0700

    finish task2

--- MyRegressor.py
+++ MyRegressor.py
@@ -4,167 +4,262 @@ from sklearn.metrics import mean_absolute_error
 import utils as ut
 import numpy as np
 import cvxpy as cp
+import math as ma
 from scipy import stats
 
 
 class MyRegressor:
-    def __init__(self, alpha,N):
-        self.n = N # num of samples 
-        # self.m = M # dimensions
-        self.weight = np.zeros((self.n, 1))
-        self.bias = 0
-        self.training_cost = 0   # N * N
+    def __init__(self, alpha):
+        self.weight = None
+        self.bias = None
+        self.training_cost = 0 # N * M
         self.alpha = alpha
-        # self.iternum = 0
-        # self.learningrate = lr
 
     def select_features(self,trainX,trainY,level):
         # Task 1-3
         feat_num = np.shape(trainX)[1]
         w = np.zeros((feat_num,2))
-        # print(feat_num)
 
         for i in range(0,feat_num):
             w[i,1] = abs(stats.pearsonr(trainY,trainX[:,i])[0])
             w[i,0] = i
-        ws = w[w[:,1].argsort()[::-1]]
-        # print(ws)
 
+        ws = w[w[:,1].argsort()[::-1]]
         sel_i = round(level * feat_num)
-        # print(level)
-        # print(sel_i)
         select_feat_index = []
 
         for j in range(0,sel_i):
             select_feat_index.append(int(ws[j,0]))
-            # select_feat_temp.append(trainX[:,int(ws[j,0])])
-            # select_feat = np.transpose(select_feat_temp)
+
         return select_feat_index # The index List of selected features
-        
-        
+         
     def select_sample(self, trainX, trainY,percent):
         # Task 1-4
-        zscore_list = np.zeros((len(trainY),2))
-        zscore_list_temp = np.abs(stats.zscore(trainY))
+        # find the difference between the predict y and y 
+        sample_num = round(percent*len(trainY))
+        PredY = trainX @ self.weight + self.bias
+        Y_error_list = np.zeros((len(trainY),2))
+        Y_error_list_temp = np.abs(trainY-PredY)
 
         for i in range(0,len(trainY)):
-            zscore_list[i,1] = zscore_list_temp[i]
-            zscore_list[i,0] = i
-        z_sort = zscore_list[zscore_list[:,1].argsort()]
-        # print(z_sort)
+            Y_error_list[i,1] = Y_error_list_temp[i]
+            Y_error_list[i,0] = i
 
-        sample_num = round(percent*len(trainY))
-        # print(sample_num)
-
+        y_sort_list = Y_error_list[Y_error_list[:,1].argsort()]
         selected_trainX = []
         selected_trainY = []
 
         for j in range(0,sample_num):
-            selected_trainX.append(trainX[int(z_sort[j,0]),:])
-            selected_trainY.append(trainY[int(z_sort[j,0])])
+            selected_trainX.append(trainX[int(y_sort_list[j,0]),:])
+            selected_trainY.append(trainY[int(y_sort_list[j,0])])
 
+        return selected_trainX, selected_trainY    # A insubset of trainX and trainY
+
+
+    def select_data(self, trainX, trainY, cost):
+        # Task 1-5
+        cost_init = len(trainY) * len(trainX[0])
+        print(len(trainX[0]))
+        if len(trainX[0]) >= 300:
+            threshold = 0.8
+
+            if cost >= threshold:
+                new_cost = round(ma.sqrt(cost),2)
+                sel_x_temp, selected_trainY = self.select_sample(trainX,trainY,new_cost)
+                sel_feat_ind = self.select_features(np.array(sel_x_temp),np.array(selected_trainY),new_cost)
+                sel_feat_num = new_cost*np.shape(trainX)[1]
+                select_trainX_temp = []
+
+                for i in range(0,round(sel_feat_num)):
+                    select_trainX_temp.append(np.array(sel_x_temp)[:,sel_feat_ind[i]])
+                    selected_trainX = np.transpose(select_trainX_temp)
+
+            elif cost < threshold:
+                sel_feat_ind2 = self.select_features(np.array(trainX),np.array(trainY),cost)
+                sel_feat_num2 = cost*np.shape(trainX)[1]
+                select_trainX_temp2 = []
+
+                for i in range(0,round(sel_feat_num2)):
+                    select_trainX_temp2.append(np.array(trainX)[:,sel_feat_ind2[i]])
+                    selected_trainX = np.transpose(select_trainX_temp2)
+                    selected_trainY = trainY
+
+        elif len(trainX[0]) < 300:
+                sel_feat_ind3 = self.select_features(np.array(trainX),np.array(trainY),cost)
+                sel_feat_num3 = cost*np.shape(trainX)[1]
+                select_trainX_temp3 = []
+
+                for i in range(0,round(sel_feat_num3)):
+                    select_trainX_temp3.append(np.array(trainX)[:,sel_feat_ind3[i]])
+                    selected_trainX = np.transpose(select_trainX_temp3)               
+                    selected_trainY = trainY
         print(np.shape(selected_trainX))
-        print(np.shape(selected_trainY))
-        
-        return selected_trainX, selected_trainY    # A subset of trainX and trainY
+        print(np.shape(selected_trainY))       
 
+        return selected_trainX, selected_trainY
 
-    # def select_data(self, trainX, trainY):
-    #     ''' Task 1-5
-    #         Todo: '''
-        
-    #     return selected_trainX, selected_trainY
-    
+    def select_testX(self, trainX, trainY, testX, cost):
+            # Task 1-5
+            if len(trainX[0]) >= 300:
+                threshold = 0.8
+
+                if cost >= threshold:
+                    new_cost = round(ma.sqrt(cost),2)
+                    sel_x_temp, selected_trainY = self.select_sample(trainX,trainY,new_cost)
+                    sel_feat_ind = self.select_features(np.array(sel_x_temp),np.array(selected_trainY),new_cost)
+                    sel_feat_num = new_cost*np.shape(trainX)[1]
+                    select_testX_temp = []
+
+                    for i in range(0,round(sel_feat_num)):
+                        select_testX_temp.append(testX[:,sel_feat_ind[i]])
+                        selected_testX = np.transpose(select_testX_temp)
+
+                elif cost < threshold:
+                    sel_feat_ind2 = self.select_features(np.array(trainX),np.array(trainY),cost)
+                    sel_feat_num2 = cost*np.shape(trainX)[1]
+                    select_testX_temp2 = []
+
+                    for i in range(0,round(sel_feat_num2)):
+                        select_testX_temp2.append(testX[:,sel_feat_ind2[i]])
+                        selected_testX = np.transpose(select_testX_temp2)
+                        selected_trainY = trainY
+
+            elif len(trainX[0]) < 300:
+                sel_feat_ind3 = self.select_features(np.array(trainX),np.array(trainY),cost)
+                sel_feat_num3 = cost*np.shape(trainX)[1]
+                select_testX_temp3 = []
+
+                for i in range(0,round(sel_feat_num3)):
+                    select_testX_temp3.append(testX[:,sel_feat_ind3[i]])
+                    selected_testX = np.transpose(select_testX_temp3)
+
+            return selected_testX 
     
     def train(self, trainX, trainY):
-        ''' 
-        Task 1-2
-        input:
-        trainX
-        trainY
-        self.alpha
+        nones = np.ones((np.shape(trainX)[0])) 
+        mones = np.ones((np.shape(trainX)[1])) 
 
-        output:
-        self.weight 
-        self.bias 
+        t = cp.Variable((np.shape(trainX)[0])) 
+        m = cp.Variable((np.shape(trainX)[1]))         
+        theta = cp.Variable(np.shape(trainX)[1]) 
+        b = cp.Variable(1) 
 
-        N = 600 used for training 
-        M = 500 used for training 
-        '''
-
-        # ti = np.zeros((self.n,1))
-        # mi = np.zeros((self.n,1))
-        # biasi = np.ones((1,1)) * self.bias
-        # x_hat = np.concatenate((self.weight, biasi, ti, mi), axis=0)
-        # print("the value of x_hat is: \n", x_hat, "\n")
-        # print("the shape of x_hat is: \n", np.shape(x_hat), "\n")
-    
-        # nones = np.ones((self.n, 1))
-        # nzeros = np.zeros((self.n, 1))
-        # nnzeros = np.zeros((self.n, self.n))
-        # I = np.eye(2)
-        # C_hat = np.concatenate((nzeros, np.zeros((1,1)), nones *(1./self.n), nones * self.alpha))
-        # print("the value of C is: \n", C_hat, "\n")
-        # print("the shape of C_hat is: \n", np.shape(C_hat), "\n")
-        
-        # A1_hat = np.concatenate((-trainX, -nones,-I,nnzeros),axis=1)
-        # A2_hat = np.concatenate((trainX, nones,-I,nnzeros),axis=1)
-        # A3_hat = np.concatenate((I,nzeros,nnzeros,-I),axis=1)
-        # A4_hat = np.concatenate((-I,nzeros,nnzeros,-I),axis=1)
-        # A_hat = np.concatenate((A1_hat,A2_hat,A3_hat,A4_hat))
-        # B_hat = np.concatenate((-trainY,trainY,nones,nones))
-
-        # print("the value of A_hat is: \n", A_hat, "\n")
-        # print("the value of B_hat is: \n", B_hat, "\n")
-        # print("the shape of A_hat * x_hat is: \n", np.shape(A_hat @ x_hat), "\n")
-        # print("the shape of B_hat is: \n", np.shape(B_hat), "\n")
-        # print(A_hat @ x_hat - B_hat)
-        # return
-
-        nones = np.ones((np.shape(trainX)[0]))
-        mones = np.ones((np.shape(trainX)[1]))
-
-        t = cp.Variable((np.shape(trainX)[0]))
-        m = cp.Variable((np.shape(trainX)[1]))
-        self.weight = cp.Variable(np.shape(trainX)[1])
-        self.bias = cp.Variable(1)
-
-        objective = cp.Minimize((1./np.shape(trainX)[0]) * nones * t + self.alpha * mones * m)
-
-        constraints = [trainY-(trainX @ self.weight + self.bias) <= t,
-                       -trainY+(trainX @ self.weight + self.bias) <= t,
-                       self.weight <= m,
-                       -self.weight <= m]
+        objective = cp.Minimize((1/np.shape(trainX)[0]) * nones @ t + self.alpha * mones @ m)
+        constraints = [trainY-(trainX @ theta + b) - t<= 0,
+                       -trainY+(trainX @ theta + b) - t<= 0,
+                       theta <= m,
+                       -theta <= m]
         prob = cp.Problem(objective, constraints)
-        result = prob.solve(verbose=True)
-        self.weight = self.weight.value
-        self.bias = self.bias.value
+        result = prob.solve(verbose=False)
+        self.weight = theta.value
+        self.bias = b.value
 
         print("the result of this LP is: \n", result, "\n")
         # print(self.weight)
 
         PredY = trainX @ self.weight + self.bias
+        # print(np.shape(PredY))
         train_error = mean_absolute_error(trainY,PredY)
-        # print("the train error is: \n", train_error, "\n")
         return train_error
     
-    # def train_online(self, trainX, trainY):
-    #     ''' Task 2 '''
+    def train_online(self, trainX, trainY, cost):
+        
+        x_set = []
+        y_set = []
+        x_tem =[]
+        y_tem = []
+        init_value_num = 10
+        weight_ini = None
+        bias_ini = None
+        train_cost = 0 
+        total_cost = 0
+        sample_num = 0
+        fea_level = np.linspace(0.01,1,10)
+        threshold = 0.2
 
-    #     # we simulate the online setting by handling training data samples one by one
-    #     for index, x in enumerate(trainX):
-    #         y = trainY[index]
+        # we simulate the online setting by handling training data samples one by one
+        for index, x in enumerate(trainX):
+            y = trainY[index]
+            x_set.append(x)
+            y_set.append(y)
+            total_cost = total_cost + len(x)
 
-    #         ### Todo:
-            
-    #     return self.training_cost, train_error
+            if index < init_value_num:
+                x_tem.append(x)
+                y_tem.append(y)
+                train_cost = train_cost + len(x)
+
+            elif index == init_value_num:
+                self.train(np.array(x_tem),np.array(y_tem))
+                train_error = []
+                weight_ini = self.weight
+                bias_ini = self.bias
+
+                for f in fea_level:
+                    self.weight = weight_ini
+                    self.bias = bias_ini
+                    feat_ind = self.select_features(np.array(x_tem),np.array(y_tem),f)
+                    ind_num = f * np.shape(x_tem)[1]
+                    select_x_tem = []
+
+                    for i in range(0, round(ind_num)):
+                        select_x_tem.append(np.array(x_tem)[:,feat_ind[i]])
+                        select_x = np.transpose(select_x_tem)
+
+                    err = self.train(np.array(select_x),np.array(y_tem))
+                    train_error.append(err)
+                    _,error = self.evaluate(np.array(select_x),np.array(y_tem))
+
+                err_list = []
+                err_list[:] = [x for x in train_error if x <= threshold * max(train_error)]
+                max_err = max(err_list)
+                best_pt = fea_level[(len(fea_level) - len(err_list) -1)]
+                self.weight = weight_ini
+                self.bias = bias_ini
+
+                if best_pt <= cost:
+                    feat_ind = self.select_features(np.array(x_tem),np.array(y_tem),cost)
+                else:
+                    feat_ind = self.select_features(np.array(x_tem),np.array(y_tem),best_pt)
+
+                select_x = np.array(x_tem)[:,feat_ind]
+                self.weight = weight_ini[feat_ind]
+                self.bias = bias_ini
+                self.feat_ind = feat_ind
+                x_tem = np.array(select_x).tolist()
+
+            else:
+                add_set = x[self.feat_ind]
+                predY = add_set @ self.weight + self.bias
+                error = abs(predY - y)
+
+                if (train_cost + len(add_set)) / total_cost <= cost and error >= 0.1 * max_err:
+                    x_tem.append(add_set)
+                    y_tem.append(y)
+                    train_cost = train_cost + len(add_set)
+                    sample_num = sample_num + 1
+
+                if sample_num / (len(y_tem) - sample_num) >= 0.1:
+                    train_error = self.train(np.array(x_tem),np.array(y_tem))
+                    sample_num = 0
+        
+        self.training_cost = train_cost / total_cost
+        _, train_error = self.evaluate(x_tem, y_tem)
+
+        return self.training_cost, train_error
+
+    def online_train_testX(self,testX,testY):
+        testX_temp = testX[:,self.feat_ind]
+        _,test_error = self.evaluate(testX_temp,testY)
+
+        return test_error
 
     def evaluate(self, X, Y):
         predY = X @ self.weight + self.bias
-        test_error = mean_absolute_error(Y, predY)
+        error = mean_absolute_error(Y, predY)
         # print("the test error is: \n", test_error, "\n")
         
-        return test_error
+        return predY, error
     
     
     def get_params(self):
--- main.py
+++ main.py
@@ -4,70 +4,141 @@ import numpy as np
 import utils as ut
 
 # test & train data 
-trainX = ut.prepare_data_gaussian()["trainX"]
-trainY = ut.prepare_data_gaussian()["trainY"]
-testX = ut.prepare_data_gaussian()["testX"]
-testY = ut.prepare_data_gaussian()["testY"]
+# data = ut.prepare_data_gaussian()
+data = ut.prepare_data_news()
 
-# test
-# print("train data X is \n", trainX, "\n")
-# print("train data Y is \n", trainY, "\n")
-# print("test data X is \n", testX, "\n")
-# print("test data Y is \n", testY, "\n")
+trainX = data["trainX"]
+trainY = data["trainY"]
+testX = data["testX"]
+testY = data["testY"]
 
-trainN = np.shape(trainX)[0]
-# trainM = np.shape(trainX)[1]
-print(np.shape(trainX))
-
+print(len(trainX[1]))
 # define alpha value 
-alpha = np.array([0,0.01,0.05,0.1,0.5,1])
+alpha = [0,0.0001,0.001,0.01,0.05,0.1,0.2,1,10,100]
 
 # 1-2 linear regressor 
-regressor_init = MyRegressor.MyRegressor(alpha[1],trainN)
+def task1_2(alpha, trainX, trainY, testX, testY):
+    train_error = []
+    test_error = []
 
-# train_error1 = regressor_init.train(trainX, trainY)
-# test_error1 = regressor_init.evaluate(testX, testY)
-# print("the train error1 is: \n", train_error1, "\n")
-# print("the test error1 is: \n", test_error1, "\n")
-# # plot1 = ut.plot_result({'taskID':'1-2', 'alpha':[alpha[0]], 'train_err':[train_error1], 'test_err':[test_error1]})
+    for i in range(0,len(alpha)):
+        regressor_init = MyRegressor.MyRegressor(alpha[i])
+        train_error.append(regressor_init.train(trainX, trainY))
+        _,test_error_temp = regressor_init.evaluate(testX, testY)
+        test_error.append(test_error_temp)
+        result_1_2 = {'taskID':'1-2', 'alpha':alpha, 'train_err':train_error, 'test_err':test_error}
 
+    error_diff = np.abs(np.array(train_error)-np.array(test_error))
+    best_alpha = alpha[np.argmin(error_diff)]
+    ut.plot_result(result_1_2)
+    return best_alpha
 
-# # 1-3 feeture Selection 
-# level = np.array([0.01,0.1,0.3,0.5,1])
-# select_feat_ind = regressor_init.select_features(trainX,trainY,level[2])
-# sel_feat_num = level[2]*np.shape(trainX)[1]
+# 1-3 feature Selection 
+def task1_3(alpha, trainX, trainY, testX, testY):
 
-# select_trainX_temp = []
-# select_testX_temp = []
-# for i in range(0,int(sel_feat_num)):
-#     select_trainX_temp.append(trainX[:,select_feat_ind[i]])
-#     select_trainX = np.transpose(select_trainX_temp)
-#     select_testX_temp.append(testX[:,select_feat_ind[i]])
-#     select_testX = np.transpose(select_testX_temp)    
-# # print(np.shape(select_trainX))
-# # print(np.shape(select_testX))
+    regressor_init = MyRegressor.MyRegressor(alpha[0])
+    level = np.array([0.01,0.1,0.2,0.3,0.5,0.8,1])
+    result_1_3 = {'taskID':'1-3', 'feat_num':[], 'train_err':[], 'test_err':[]}
 
-# train_error2 = regressor_init.train(select_trainX, trainY)
-# test_error2 = regressor_init.evaluate(select_testX, testY)
-# # print("the train error2 is: \n", train_error2, "\n")
-# # print("the test error2 is: \n", test_error2, "\n")
-# # plot2 = ut.plot_result({'taskID':'1-3', 'feat_num':[0.3], 'train_err':[train_error2], 'test_err':[test_error2]})
+    for j in range(0,len(level)):
+        result_1_3['feat_num'].append(level[j])
+        select_feat_ind = regressor_init.select_features(trainX,trainY,level[j])
+        sel_feat_num = level[j]*np.shape(trainX)[1]
+        select_trainX_temp = []
+        select_testX_temp = []
 
+        for i in range(0,round(sel_feat_num)):
+            select_trainX_temp.append(trainX[:,select_feat_ind[i]])
+            select_trainX = np.transpose(select_trainX_temp)
+            select_testX_temp.append(testX[:,select_feat_ind[i]])
+            select_testX = np.transpose(select_testX_temp)    
+        
+        train_error2 = regressor_init.train(select_trainX, trainY)
+        _,test_error2 = regressor_init.evaluate(select_testX, testY)
+        result_1_3['train_err'].append(train_error2)
+        result_1_3['test_err'].append(test_error2)
+    
+    ut.plot_result(result_1_3)
 
-# 1-4 Sample Selection
-percent = np.array([0.01,0.1,0.3,0.5,1])
-trainset = regressor_init.select_sample(trainX,trainY,percent[1])
-sel_trainX = np.array(trainset[0])
-sel_trainY = np.array(trainset[1])
+# 1-4 Sample Selection 
+def task1_4(alpha, trainX, trainY, testX, testY):
+    regressor_init = MyRegressor.MyRegressor(alpha[0])
+    regressor_init.train(trainX, trainY)
+    percent = [0.01,0.1,0.3,0.5,0.8,0.9,1]
+    train_error3 = []
+    test_error3 = []
 
+    for i in range(0,len(percent)):
+        trainset = regressor_init.select_sample(trainX,trainY,percent[i])
+        sel_trainX = np.array(trainset[0])
+        sel_trainY = np.array(trainset[1])
+        train_error3.append(regressor_init.train(sel_trainX, sel_trainY))
+        _,test_error3_temp = regressor_init.evaluate(testX, testY)
+        test_error3.append(test_error3_temp)    
+        result_1_4 = {'taskID':'1-4', 'sample_num':percent, 'train_err':train_error3, 'test_err':test_error3}
 
-redN = round(percent[1]*len(trainY))
-print(redN)
-regressor_redN = MyRegressor.MyRegressor(alpha[4],redN)
-print(np.shape(sel_trainX))
-print(np.shape(sel_trainY))
-train_error3 = regressor_redN.train(sel_trainX, sel_trainY)
-# test_error3 = regressor_redN.evaluate(select_testX, testY)
+    ut.plot_result(result_1_4)
+
+# 1-5 Sample and Feature Selection
+def task1_5(alpha, trainX, trainY, testX, testY):
+    result_1_5 = {'taskID':'1-5', 'cost':[], 'train_err':[], 'test_err':[]}
+    regressor_init = MyRegressor.MyRegressor(alpha[0])
+    cost = [0.01,0.1,0.3,0.5,0.8,0.9,1]
+    train_error4 = []
+    test_error4 = []
+
+    for i in range(0,len(cost)):
+        regressor_init.train(trainX, trainY)
+        trainset2 = regressor_init.select_data(trainX,trainY,cost[i])
+        sel_testX = regressor_init.select_testX(trainX,trainY,testX,cost[i])
+        sel_data_trainX = np.array(trainset2[0])
+        sel_data_trainY = np.array(trainset2[1])
+        train_error4.append(regressor_init.train(sel_data_trainX, sel_data_trainY))
+        _,test_error4_temp = regressor_init.evaluate(sel_testX, testY)
+        test_error4.append(test_error4_temp)
+
+        result_1_5 = {'taskID':'1-5', 'cost':cost, 'train_err':train_error4, 'test_err':test_error4}
+
+    ut.plot_result(result_1_5)
+
+# Task 2 
+def task2(alpha, trainX, trainY, testX, testY):
+    regressor_init = MyRegressor.MyRegressor(alpha[0])
+    percentage = [0.01,0.1,0.3,0.5,0.8,0.9,1]
+    train_error5 = []
+    test_error5 = []
+    training_cost = []
+
+    for i in range(0,len(percentage)):
+        trainset3 = regressor_init.train_online(trainX, trainY, percentage[i])
+        test_error = regressor_init.online_train_testX(testX, testY)
+        training_cost.append(trainset3[0])
+        train_error5.append(trainset3[1])
+        test_error5.append(test_error)
+        
+    result2 = {'taskID':'2', 'cost':training_cost, 'train_err':train_error5, 'test_err':test_error5}
+    ut.plot_result(result2)
+
+# Main Functions
+# Uncomment each command to run the file 
+
+# 1-2
+# sel_alpha = task1_2(alpha, trainX, trainY, testX, testY)
+
+# optimized a = 0.05 for gasussian set
+# optimized a = 0 for news set
+
+# 1-3
+# task1_3(alpha, trainX, trainY, testX, testY)
+
+# 1-4
+# task1_4(alpha, trainX, trainY, testX, testY)
+
+# 1-5
+# task1_5(alpha, trainX, trainY, testX, testY)
+
+# 2-1
+task2(alpha, trainX, trainY, testX, testY)
 
 
 
